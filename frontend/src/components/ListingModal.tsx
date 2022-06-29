a4484ac32765353977f973c32599eea14b1eba13
Eric Le <therealericle@gmail.com>
2022-06-29 13:21:29 -0400

    make close button grey

--- frontend/src/components/ListingModal.tsx
+++ frontend/src/components/ListingModal.tsx
@@ -229,7 +229,7 @@ export const ListingModal = ({
                   </button>
                   <button
                     type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-gray-100 px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-500 focus-visible:ring-offset-2"
                     onClick={onClose}
                   >
                     Close
